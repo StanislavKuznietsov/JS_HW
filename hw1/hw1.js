2800d00f0981d1c2a745376e94b8b286de95259e
StanislavKuznietsov <stanislavkuznietsov@2015gmail.com>
2021-09-12 22:09:27 +0300

    hw1_

---
+++ hw1/hw1.js
@@ -0,0 +1,29 @@
+// const name = "Vasya";
+
+// if (typeof name == "string") {
+//     console.log("Hello " + name)
+// } else {
+//     console.log("Wrong Data type")
+// }
+
+// let checkName = typeof name == "string" ? console.log("Hello " + name) : console.log("Wrong Data type");
+
+
+
+
+// const name = 123;
+
+// if (typeof name == "string") {
+//     console.log("Hello" + name)
+// } else {
+//     console.log("Wrong Data type")
+// }
+
+
+
+// console.log(typeof 1);
+// console.log(typeof "");
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof {});
